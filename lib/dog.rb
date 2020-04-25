1cf1ebf6bd737ee82fc0e9950693ded20204f650
Koray Ozkal <koryozkal@gmail.com>
2020-04-25 00:30:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,12 +7,26 @@ def name= (dogs_name)
   def name
     @name
   end
+  
+  def breed= (dogs_breed)
+    @breed = dogs_breed
+    
+    def breed
+      def @breed 
+      end 
+      
  end  
  
 fido = Dog.new
 fido.name = "Fido"
  
 puts fido.name
+
+snoopy = Dog.new 
+snoopy.name = "snoopy"
+snoopy.breed = "Beagle"
+puts. snoopy.name
+puts. snoopy.breed
 
 
 
