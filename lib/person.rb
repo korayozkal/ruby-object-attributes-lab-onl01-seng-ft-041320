5d6fe13ec9e1ee4d0adac6452bfeb0fa571cbcf0
Koray Ozkal <koryozkal@gmail.com>
2020-04-25 00:41:29 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,7 @@
 class Person
-  
-  def name = (person_name)
-  @name = person_name
-  end 
-  
-  def name 
-    @name 
-  end 
-  
-end
+  def name=(person_name)
+    @name = person_name
+  end
+  def name
+    @name
+  end
